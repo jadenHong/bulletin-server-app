632c503a49a26d4efd38d7855f230e6b32dc3c10
jadenHong <62584142+jadenHong@users.noreply.github.com>
2020-08-12 23:26:35 -0600

    React: node.js
    bulletin-server side.
    connect between mysql and front side

--- src/index.js
+++ src/index.js
@@ -1,17 +1,117 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// npm install mysql2
+// npm install express
+// npm install i -D nodemon
+// npm install cors
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// connect server
+console.log('Hong\'s server');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+// connect database 
+const pool = mysql.createPool({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'ap28xm59',
+  database: 'bulletin_practice',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const promisePool = pool.promise();
+
+const app = express();
+const SERVER_PORT = 8090;
+app.use(express.json()); // json 바디를 파싱해준다.
+// app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cors());
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is listening to ${SERVER_PORT}`);
+});
+
+// ****** Read selected data ******
+
+// connect browser to server. 브라우저에서 아래의 URL로 fetch 하면 이 쪽으로 get이 된다.
+app.get('/customer/search/:name', async (req, res) => {
+  const name = req.params.name;
+  console.log(`received: ${name}`);
+  try {
+    const result = await getInfoByName(name);
+    console.log(`result: ${JSON.stringify(result)}`);
+    console.log(`result: ${result[0].name}`);
+    res.json(result) // response 에서 브라우저로 넘겨준다. 그래서 fetch().then((response) => response.json()) 여기서 response를 받는것이다.
+  } catch (err) {
+    throw err;
+  }
+});
+
+const getInfoByName = async (name) => {
+  // 절대로 이렇게 쿼리 짜지마 큰일남. 파라미터로 받을떄 특히!! 작동은 잘되는데 sql injection 이라고하는 mysql 해킹방식에 노출됨
+  // const QUERY = `SELECT * FROM userinfo WHERE name=${name}`
+
+  const QUERY = `SELECT * FROM userinfo WHERE name=?`; // raw query 라고 부름
+  const [rows, fields] = await promisePool.query(QUERY, name);
+  console.log(`rows: ${rows}`);
+  console.log(`fileds: ${fields}`);
+  return rows;
+}
+
+// ****** Read all data ******
+app.get('/customer/information/', async (req, res) => {
+  try {
+    const result = await getCustInfo();
+    res.json(result);
+  } catch (error) {
+    throw error;
+  }
+});
+
+const getCustInfo = async () => {
+  const QUERY = `SELECT name, gender, email,₩₩ title, content FROM userinfo NATURAL JOIN board`;
+  const [rows, fileds] = await promisePool.query(QUERY);
+  return rows;
+}
+
+
+// ****** Update data ******
+app.get('/customer/change/:userid', async (req, res) => {
+  const userId = req.params.userid;
+  try {
+    const result = await modifyByUserid(userId);
+    res.json(result);
+  } catch (error) {
+    throw error;
+  }
+});
+
+const modifyByUserid = async (userId) => {
+  const QUERY = `UPDATE board SET content ='React Developer' WHERE userid = ?`;
+  const [rows] = await promisePool.query(QUERY, userId);
+  return rows;
+};
+
+// ****** Insert data ******
+
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/customer/save/', async (req, res) => {
+  const body = req.body;
+  console.log(req.body.name);
+  try {
+    const result = await saveDataByUserid(body);
+    res.json(result);
+  } catch (error) {
+    throw error;
+  }
+});
+
+const saveDataByUserid = async (body) => {
+  const QUERY = 'INSERT INTO userinfo VALUES(?,?,?,?,?)';
+  const [rows] = await promisePool.query(QUERY, [body.id, body.passwd, body.name, body.gender, body.email]);
+  return rows;
+}
